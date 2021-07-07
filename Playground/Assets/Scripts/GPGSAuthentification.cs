e22a3eb5c577b56233670c48159ecb463479cb61
codewoerks <code@paulwoerner.com>
2021-07-07 16:23:14 +0200

    Google Play Services
    I successfully implemented Google Play Services.

---
+++ Playground/Assets/Scripts/GPGSAuthentification.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class GPGSAuthentification : MonoBehaviour
+{
+    public static PlayGamesPlatform platform;
+
+    void Start()
+    {
+        if (platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+                
+                .Build();
+
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+
+            PlayGamesPlatform.Activate();
+            Debug.Log("Play Services: Initializing");
+
+            Authenticate();
+        }
+    }
+
+    public void Authenticate()
+    {
+        Social.Active.localUser.Authenticate(success =>
+        {
+            Debug.Log("Play Services: " + success.ToString());
+            if (success)
+            {
+                Debug.Log("Logged in successfully");
+            }
+            else
+            {
+                Debug.LogError("Failed to log in :(");
+            }
+        });
+    }
+}
---
+++ Playground/Assets/Scripts/UIConsole.cs
@@ -0,0 +1,108 @@
+﻿// Code by Paul Woerner - 2021
+// info@paulwoerner.com
+
+#region Info
+// Shows a UI console for debugging on Android
+#endregion
+
+#region Libraries
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+using System;
+#endregion
+
+#region Classes
+[System.Serializable]
+public class LogStyle
+{
+    public string tag;
+    public Color txtColor;
+}
+#endregion
+
+public class UIConsole : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] LogStyle[] logStyles;
+
+    [SerializeField] int maxCharacters = 4098;
+
+    string clipboard = "";
+    static string myLog = "";
+    string myType;
+
+    [SerializeField] Transform content;
+    [SerializeField] GameObject logPrefab;
+    [SerializeField] List<GameObject> logObjects;
+    [SerializeField] ScrollRect scrollRect;
+    #endregion
+
+    #region Update Log
+    void UpdateLog(string logTxt, string stackTrace, LogType logType)
+    {
+        string log = logTxt;
+
+        myLog = logTxt + "\n" + myLog;
+
+        #region Set TimeStamp
+        DateTime dateTime = DateTime.Now;
+        string timeStamp = "[" + dateTime.Hour.ToString() + ":" + dateTime.Minute.ToString() + ":" + dateTime.Second.ToString() + "] ";
+        #endregion
+
+        GameObject txtObj = Instantiate(logPrefab, content);
+        TextMeshProUGUI tm = txtObj.GetComponent<TextMeshProUGUI>();
+        tm.color = logStyles[(int)logType].txtColor;
+
+        myType = timeStamp + logStyles[(int)logType].tag;
+        tm.SetText(myType + ": " + log);
+
+        logObjects.Add(txtObj);
+
+        clipboard += myType + log + "\n";
+
+        ScrollToBottom();
+    }
+    #endregion
+
+    #region Clipboard
+    public void GetClipBoard()
+    {
+        string copyString = "Console Clipboard: \n";
+
+        string[] subLines = clipboard.Split('\n');
+
+        for (int i = subLines.Length - 1; i > 0; i--)
+        {
+            copyString += subLines[i];
+            copyString += "\n";
+            if (copyString.Length >= maxCharacters)
+            {
+                break;
+            }
+        }
+
+        GUIUtility.systemCopyBuffer = copyString;
+    }
+    #endregion
+
+    #region Scroll
+    public void ScrollToBottom()
+    {
+        scrollRect.normalizedPosition = new Vector2(0, 0);
+    }
+    #endregion
+
+    #region Setup
+    void OnEnable()
+    {
+        Application.logMessageReceived += UpdateLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= UpdateLog;
+    }
+    #endregion
+}
